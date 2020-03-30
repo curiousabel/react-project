4b74315a40259a96ac8319def53e3a51353ff4c6
KingAbel <abelemun1@gmail.com>
2020-03-30 00:33:16 -0400

    autosuggestion

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import MaterialUIEx1 from './week7/materialuiex1';
+import MaterialUIEx2Component from './week7/materialuiex2';
+ReactDOM.render(<MaterialUIEx2Component />, document.getElementById('root'));
