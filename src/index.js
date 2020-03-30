a0f9ed90e2192e1221d969a06a3aee61b2a48e3e
KingAbel <abelemun1@gmail.com>
2020-03-30 13:00:27 -0400

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MaterialUIEx1 from './week7/materialuiex1';
-import MaterialUIEx2Component from './week7/materialuiex2';
-ReactDOM.render(<MaterialUIEx2Component />, document.getElementById('root'));
+
+
+
+import MaterialUIEx3Component from './week7/materialuiex3';
+
+ReactDOM.render(<MaterialUIEx3Component />, document.getElementById('root'));
--- src/theme.js
+++ src/theme.js
@@ -1,4 +1,5 @@
 import { createMuiTheme } from '@material-ui/core/styles';
+
 export default createMuiTheme({
 typography: {
 useNextVariants: true
